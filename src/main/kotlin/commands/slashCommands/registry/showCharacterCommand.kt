8bed35bf0b311afd272e54b6bba44c248c8a9ed1
klosradieschen <nicolas@wagener.family>
2026-06-14 00:30:08 +0200

    mini fix

--- src/main/kotlin/commands/slashCommands/registry/showCharacterCommand.kt
+++ src/main/kotlin/commands/slashCommands/registry/showCharacterCommand.kt
@@ -7,13 +7,13 @@ import database.Database
 import database.tables.CharacterEntity
 import database.tables.CharacterTable
 import dev.kord.common.Color
+import dev.kord.common.entity.Snowflake
 import dev.kord.core.behavior.interaction.response.respond
 import dev.kord.rest.builder.interaction.string
 import dev.kord.rest.builder.interaction.user
 import dev.kord.rest.builder.message.embed
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toInstant
-import org.jetbrains.exposed.v1.core.and
 import org.jetbrains.exposed.v1.core.eq
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 import kotlin.time.Instant
@@ -38,11 +38,12 @@ object ShowCharacterCommand : SlashCommand (
 		val sanitizedName = name.lowercase().replace(" ", "_")
 
 		val character = transaction(Database.db) {
-			CharacterEntity.find { CharacterTable.ownerID eq user.id.value and (CharacterTable.sanitizedName eq sanitizedName) }.singleOrNull()
+			CharacterEntity.find { CharacterTable.sanitizedName eq sanitizedName }.singleOrNull()
 		}
 
 		if (character == null) {
 			Hank.error<NoSuchElementException>(
+				response,
 				"Character not found",
 				"""
 					We are in the command "show-character" which you can use to see your or someone else's character given their name.
@@ -70,8 +71,8 @@ object ShowCharacterCommand : SlashCommand (
 					}
 
 					author {
-						this.name = user.asMember(Config.Snowflakes.ahaGuildID).effectiveName
-						icon = user.avatar?.cdnUrl?.toUrl()
+						this.name = kord.getUser(Snowflake(character.ownerID))!!.asMember(Config.Snowflakes.ahaGuildID).effectiveName
+						icon = kord.getUser(Snowflake(character.ownerID))!!.avatar?.cdnUrl?.toUrl()
 					}
 
 					timestamp = Instant.fromEpochMilliseconds(character.createdAt.toInstant(TimeZone.UTC).toEpochMilliseconds())
